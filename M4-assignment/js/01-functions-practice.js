ec249793928cdfb67ca9f019cc5f6175e53d70dc
Brian J <Brian.jones92@gmail.com>
2026-06-30 16:36:47 -0700

    Functions Practice Complete

--- M4-assignment/js/01-functions-practice.js
+++ M4-assignment/js/01-functions-practice.js
@@ -7,11 +7,22 @@ function halfNumber() {
 console.log(`Half of ${num1} is ${halfNumber()}`)
 
 //STEP 2
-function percentOf() {
-
+let num1 = prompt("Enter a number between 1 and 10")
+function squareNumber() {
+    return num1 * num1
 }
+console.log(`The result of squaring the number ${num1} is ${squareNumber()}`)
 //STEP 3
-
+let num1 = prompt("Enter a number between 1 and 10")
+function percentOf() {
+    return num1 * .5
+}
+console.log(`${percentOf()} is 50% of ${num1}`)
 //STEP 4
-
+let num1 = prompt("Enter a number between 1 and 10")
+let num2 = prompt("Enter another number between 1 and 10")
+function findModulus() {
+    return num1 % num2 
+}
+console.log(`${findModulus(parseInt())} is the modulus of ${num2} and ${num1}`)
 //STEP 5
