28545aa97ae5fb8e1da2b066c6b06a76d5ce3af2
Brian J <Brian.jones92@gmail.com>
2026-06-30 16:15:15 -0700

    File Upload

---
+++ M4-assignment/js/01-functions-practice.js
@@ -0,0 +1,17 @@
+//STEP 1
+let num1 = prompt("Enter a number between 1 and 10")
+let num2 = 2
+function halfNumber() {
+    return num1 / num2
+}
+console.log(`Half of ${num1} is ${halfNumber()}`)
+
+//STEP 2
+function percentOf() {
+
+}
+//STEP 3
+
+//STEP 4
+
+//STEP 5
